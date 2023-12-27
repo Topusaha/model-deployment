2011ee2c458142b8409e5d9d77d248c168c18fc5
Topu Saha <topusaha@Topus-MacBook-Air.local>
2023-12-27 16:13:31 -0500

    ML Model connected

--- app.py
+++ app.py
@@ -1,14 +1,29 @@
 from flask import Flask, render_template, request
+import pickle
+
+cv = pickle.load(open('models/cv.pkl', 'rb'))
+model = pickle.load(open('models/clf.pkl', 'rb'))
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/")
 def home():
-    email = ""
-    if request.method == "POST":
-        email = request.form.get('Email-Content')
+    return render_template('index.html')
 
-    return render_template('index.html', text=email)
+
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    email_text = request.form.get('Email-Content')
+
+    tokenized_email = cv.transform([email_text])
+    prediction = model.predict(tokenized_email)
+    output = "This email does not seem like a spam"
+
+    if prediction == 1:
+        output = "This email seems like a spam" 
+
+    return render_template('index.html', predictions = output, text=email_text)
 
 
 if __name__ == "__main__":
